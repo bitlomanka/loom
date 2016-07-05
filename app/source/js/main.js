79f2f9d4d9c05dc32e3b3cf48b9242c532afc766
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-07-05 15:36:01 +0300

    page structure

--- app/source/js/main.js
+++ app/source/js/main.js
@@ -1,7 +1,32 @@
 
 $(document).ready(function(){
+    
+    $('.slider_bg').find('.owl-carousel').owlCarousel({
+        items:1,
+        singleItem:true,
+        itemsTabletSmall:false,
+        navigation: true,
+        navigationText: false,
+        slideSpeed: 400,
+        autoPlay: 4000
+    });
 
-  
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        singleItem:true,
+        itemsTabletSmall:false,
+        navigation: true,
+        navigationText: false,
+        slideSpeed: 400
+    });
+    
+    
+    
+    $('.img-holder').imageScroll({
+        holderMinHeight: 405,
+        holderMaxHeight: 500,
+        speed: 0.3
+    });
     
     
 });
